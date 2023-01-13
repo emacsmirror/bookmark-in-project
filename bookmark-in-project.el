a07c178ff3c7e8a22d1aacab54f5945190d9508d
Campbell Barton <ideasman42@gmail.com>
2023-01-13 23:54:25 +1100

    Fix compatibility for pos-bol/eol for emacs <29.1

--- bookmark-in-project.el
+++ bookmark-in-project.el
@@ -32,6 +32,18 @@
 
 
 ;; ---------------------------------------------------------------------------
+;; Compatibility
+
+(when (version< emacs-version "29.1")
+  (defsubst pos-bol (&optional n)
+    "Return the position at the line beginning."
+    (line-beginning-position n))
+  (defsubst pos-eol (&optional n)
+    "Return the position at the line end."
+    (line-end-position n)))
+
+
+;; ---------------------------------------------------------------------------
 ;; Custom Variables
 
 (defgroup bookmark-in-project nil
