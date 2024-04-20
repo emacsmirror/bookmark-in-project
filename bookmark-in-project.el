3e342214364afd5cb4b5b0e0acfab91599998b8e
Campbell Barton <ideasman42@gmail.com>
2024-04-20 21:04:49 +1000

    Bump emacs version to 29.1

--- bookmark-in-project.el
+++ bookmark-in-project.el
@@ -8,7 +8,7 @@
 ;; URL: https://codeberg.org/ideasman42/emacs-bookmark-in-project
 ;; Keywords: convenience
 ;; Version: 0.2
-;; Package-Requires: ((emacs "27.1"))
+;; Package-Requires: ((emacs "29.1"))
 
 ;;; Commentary:
 
@@ -29,22 +29,6 @@
 (require 'bookmark)
 ;; Use for creating text for the context at the current point.
 (require 'imenu)
-
-
-;; ---------------------------------------------------------------------------
-;; Compatibility
-
-(when (and (version< emacs-version "29.1") (not (and (fboundp 'pos-bol) (fboundp 'pos-eol))))
-  (defun pos-bol (&optional n)
-    "Return the position at the line beginning."
-    (declare (side-effect-free t))
-    (let ((inhibit-field-text-motion t))
-      (line-beginning-position n)))
-  (defun pos-eol (&optional n)
-    "Return the position at the line end."
-    (declare (side-effect-free t))
-    (let ((inhibit-field-text-motion t))
-      (line-end-position n))))
 
 
 ;; ---------------------------------------------------------------------------
